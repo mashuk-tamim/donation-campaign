381970d62bd6d8d536a18f63b2bf628aaeefba30
mashuk-tamim <tamimmashuk328@gmail.com>
2023-09-26 16:43:25 +0600

    donation page completed

--- src/components/Donation/Donation.jsx
+++ src/components/Donation/Donation.jsx
@@ -1,5 +1,6 @@
 
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 Donation.propTypes = {
--- src/pages/Donated/Donated.jsx
+++ src/pages/Donated/Donated.jsx
@@ -1,14 +1,69 @@
 
+import { useEffect, useState } from 'react';
 import DonatedDetails from '../DonatedDetails/DonatedDetails';
 
 const Donated = () => {
 
+    const [showDonation, setShowDonation] = useState(4);
+
+    const [seeAll, setSeeAll] = useState(false);
+
     const donations = JSON.parse(localStorage.getItem('donation'));
-    console.log(donations);
+
+    console.log(donations)
+
+    const [donationLength, setDonationLength] = useState(0)
+
+
+    useEffect(() => {
+        if (donations) {
+            if (donations.length > 4) {
+                console.log(donations.length)
+                setShowDonation(showDonation);
+            }
+            else {
+                setShowDonation(donations.length);
+            }
+
+            if (seeAll) {
+                setShowDonation(donations.length)
+            }
+            else {
+                setShowDonation(4);
+            }
+            setDonationLength(donations.length)
+        }
+        else {
+            setDonationLength(donationLength);
+            console.log('donation length', donationLength);
+        }
+
+    }, [showDonation, donationLength, donations, seeAll])
+
     return (
-        <div className='grid grid-cols-1 lg:grid-cols-2 gap-5'>
+        <div>
             {
-                donations.map(donation => <DonatedDetails donation={donation} key={donation.id}></DonatedDetails>)
+                donationLength ?
+                    <div className='grid grid-cols-1 lg:grid-cols-2 gap-5'>
+                        {
+                            donations?.slice(0, showDonation)?.map(donation => <DonatedDetails donation={donation} key={donation.id}></DonatedDetails>)
+                        }
+                        {
+                            donationLength > 4 ?
+                                <div className='flex justify-center'>
+                                    <button
+                                        onClick={() => { setSeeAll(!seeAll) }}
+                                        className='text-white bg-[#009444] text-xs md:text-base lg:text-xl font-semibold py-2 px-3 lg:px-5 rounded-md'>{
+                                            seeAll ? 'See less' : 'See all'
+                                        }</button>
+                                </div>
+                                :
+                                <div></div>
+                        }
+
+                    </div>
+                    :
+                    <p className="text-4xl font-semibold h-[80vh] flex items-center justify-center">You have not donated yet!</p>
             }
         </div>
     );
